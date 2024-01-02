c9211743f1aa697eb261902fa4bc1bdf5eea3f6c
Idan Falach <idan.falach@post.runi.ac.il>
2024-01-02 23:47:16 +0200

    UniqueChars exercise

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,17 +1,27 @@
-/** String processing exercise 2. */
+/**
+ * String processing exercise 2.
+ */
 public class UniqueChars {
-    public static void main(String[] args) {  
+    public static void main(String[] args) {
         String str = args[0];
-        System.out.println(uniqueChars(str));
+        System.out.println(uniqueChars("a quick brown fox"));
     }
 
     /**
-     * Returns a string which is identical to the original string, 
+     * Returns a string which is identical to the original string,
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String uniqueCharsString = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char character = s.charAt(i);
+            if (uniqueCharsString.indexOf(character) == -1 || character == ' ') {
+                uniqueCharsString += character;
+            }
+        }
+
+        return uniqueCharsString;
     }
 }
